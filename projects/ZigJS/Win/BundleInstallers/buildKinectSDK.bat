fa9493a9bdfffd3a72168b026c681fe271885b1a
Roee Shenberg <roee@zigfu.com>
2012-02-20 20:05:25 -0800

    added sleeps during the bundle signatures because it failed without them a bunch of times on my system and I don't see a "right" way of fixing this :(

--- projects/ZigJS/Win/BundleInstallers/buildKinectSDK.bat
+++ projects/ZigJS/Win/BundleInstallers/buildKinectSDK.bat
@@ -2,6 +2,12 @@ candle installerKinectSDK.wxs
 IF %ERRORLEVEL% GTR 0 goto error
 light -ext WixBalExtension -o %1\ZigJSKinectSDK.exe installerKinectSDK.wixobj
 IF %ERRORLEVEL% GTR 0 goto error
+echo sleeping for 5 seconds to ensure ZigJSOpenNI.exe is not 
+REM creating a vbs script to sleep for 1 second because otherwise signing fails
+> "%Temp%.\sleep.vbs" ECHO WScript.Sleep 5000
+CSCRIPT //NoLogo "%Temp%.\sleep.vbs"
+DEL "%Temp%.\sleep.vbs"
+
 REM thank you internet for this "variable" hack
 set /p passphrase= <../../../../../codesign/passphrase.txt
 

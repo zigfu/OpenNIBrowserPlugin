7e07b8115bdcfd0ca675bff2945e71e3ce17c6e3
Roee Shenberg <roee@zigfu.com>
2012-02-28 20:15:33 -0800

    added labelmap-related stuff to sensor object

--- projects/ZigJS/Sensor.h
+++ projects/ZigJS/Sensor.h
@@ -15,7 +15,7 @@ public:
 	static SensorPtr CreateSensor();
 	virtual ~Sensor();
 
-	virtual bool ReadFrame(bool updateDepth, bool updateImage, bool isWebplayer) = 0; //true if there is new data, false otherwise
+	virtual bool ReadFrame(bool updateDepth, bool updateImage, bool updateLabelMap) = 0; //true if there is new data, false otherwise
 
 	virtual bool Valid() const = 0;
 
@@ -31,13 +31,15 @@ public:
 	// TODO: refactor
 	const FB::variant& GetImage() const { return m_imageJS; }
 	const FB::variant& GetDepth() const { return m_depthJS; }
+	const FB::variant& GetLabelMap() const { return m_labelMapJS; }
 	//const std::wstring& GetImage() const { return m_imageBuffer; }
 	//const std::wstring& GetDepth() const { return m_depthBuffer; }
 	Sensor() {
 		//m_depthBuffer.resize(MAP_XRES*MAP_YRES*2); //16-bpp
 		//m_imageBuffer.resize(MAP_XRES*MAP_YRES*3); //24-bpp
 		m_depthBuffer.resize(MAP_XRES*MAP_YRES*2*4/3); //16-bpp, base64
-		//implicit assumption that the division by 3 will be round, which works because of the aspect ratio (4x3)
+		m_labelMapBuffer.resize(MAP_XRES*MAP_YRES*2*4/3); //implicit assumption that the division by 3 will be round, which works because of the aspect ratio (4x3)
+		
 		m_imageBuffer.resize(MAP_XRES*MAP_YRES*4); //24-bpp, base64
 	}
 	static void Unload();
@@ -45,8 +47,10 @@ public:
 protected:
 	FB::variant m_imageJS;
 	FB::variant m_depthJS;
+	FB::variant m_labelMapJS;
 	std::string m_imageBuffer;
 	std::string m_depthBuffer;
+	std::string m_labelMapBuffer;
 
 
 	//base64 stuff

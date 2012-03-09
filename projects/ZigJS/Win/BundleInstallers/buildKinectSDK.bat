1113190f129d84ba4a8758cda98147dfd15fae3c
Roee Shenberg <roee@zigfu.com>
2012-03-09 15:59:14 -0800

    re-added sleep to bundle signature process :/

--- projects/ZigJS/Win/BundleInstallers/buildKinectSDK.bat
+++ projects/ZigJS/Win/BundleInstallers/buildKinectSDK.bat
@@ -9,6 +9,8 @@ REM unpack bootstrapper engine, sign it and then place the signed version in the
 insignia -ib %1\ZigJSKinectSDK_unsigned.exe -o engine.exe
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\signtool.exe" sign /f ../../zigcert.pfx /p %passphrase% /t http://timestamp.digicert.com/ engine.exe
 insignia -ab engine.exe %1\ZigJSKinectSDK_unsigned.exe -o %1\ZigJSKinectSDK.exe
+REM sleep for 1 sec because insignia doesn't shut down properly
+ping 127.0.0.1 -n 2 -w 1000
 REM sign the bundle with the signed bootstrapper
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\signtool.exe" sign /f ../../zigcert.pfx /p %passphrase% /t http://timestamp.digicert.com/ %1\ZigJSKinectSDK.exe
 IF %ERRORLEVEL% GTR 0 goto error

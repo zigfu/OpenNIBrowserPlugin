53d11e9f78bff5a2a855fb4b3e2ffbb927047c14
Roee Shenberg <roee@zigfu.com>
2012-03-12 14:34:24 -0700

    bumped version to 0.9.9

--- projects/ZigJS/PluginConfig.cmake
+++ projects/ZigJS/PluginConfig.cmake
@@ -11,7 +11,7 @@ set(COMPANY_NAME "ZigFu")
 
 # ActiveX constants:
 set(FBTYPELIB_NAME ZigJSLib)
-set(FBTYPELIB_DESC "ZigJS 0.98 Type Library")
+set(FBTYPELIB_DESC "ZigJS 0.99 Type Library")
 set(IFBControl_DESC "ZigJS Control Interface")
 set(FBControl_DESC "ZigJS Control Class")
 set(IFBComJavascriptObject_DESC "ZigJS IComJavascriptObject Interface")
@@ -34,7 +34,7 @@ set(MOZILLA_PLUGINID "zigfu.com/ZigJS")
 # strings
 set(FBSTRING_CompanyName "ZigFu")
 set(FBSTRING_PluginDescription "Motion control in the browser")
-set(FBSTRING_PLUGIN_VERSION "0.9.8")
+set(FBSTRING_PLUGIN_VERSION "0.9.9")
 set(FBSTRING_LegalCopyright "Copyright 2012 Motion Arcade Inc.")
 set(FBSTRING_PluginFileName "np${PLUGIN_NAME}v${FBSTRING_PLUGIN_VERSION}.dll")
 set(FBSTRING_ProductName "ZigJS")

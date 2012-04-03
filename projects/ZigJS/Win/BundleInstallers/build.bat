158d181efefa745966ccf17b7f1042f4c2267d1f
Roee Shenberg <roee@zigfu.com>
2012-04-02 22:04:05 -0700

    bumped version

--- projects/ZigJS/Win/BundleInstallers/build.bat
+++ projects/ZigJS/Win/BundleInstallers/build.bat
@@ -2,7 +2,7 @@
 REM usage: param 1 should be path to the MSI to include, param 2 should be
 REM        the output directory with no trailing slash
 REM example usage from my system:
-REM build.bat ..\..\..\..\build\bin\ZigJS\Release\ZigJSv0.9.10.msi ..\..\..\..\build\bin\ZigJS\Release
+REM build.bat ..\..\..\..\build\bin\ZigJS\Release\ZigJSv1.0.0.msi ..\..\..\..\build\bin\ZigJS\Release
 
 REM set the path to the source MSI
 set ZIGJSMSI=%1
